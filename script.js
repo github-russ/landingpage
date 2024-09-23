e3008d2ecef8976669e6a2d1774d8b03d831f00c
github-russ <cacdacdave@yahoo.com>
2024-09-23 23:33:48 +0800

    Final script.js

--- script.js
+++ script.js
@@ -12,16 +12,18 @@ header1.textContent = "Welcome to EasyPC!";
 header1.style.fontFamily = 'Franklin Gothic Medium, Arial Narrow, Arial, sans-serif';
 
 // Add a button and event listener for button clicks.
-function sayThankYou() {
+function sayThankYou(event) {
+    event.preventDefault(); 
 
     let emailInput = document.querySelector('#email');
 
     if (emailInput.value.trim() === '') {
         alert("Please fill in required fields.");
-    } else if (!emailInput.value.includes('@') || !emailInput.value.includes('.com')) {
-        alert("Please complete your email address.");
+    } else if (emailInput.value.includes('@') && emailInput.value.includes('.com')) {
+        alert("You will now get the latest updates and promotions from EasyPC!");
+        window.location.href = 'static-page.html';
     } else {
-        alert("You will now get latest updates and promotions from EasyPC!");
+        alert("Please enter a valid email address.");
     }
 }
 
